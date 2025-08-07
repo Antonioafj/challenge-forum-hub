4bfa60c0a8fea5926cf1249c5acd4d68650d8f3e
Antonioafj <antonioafj.edu@gmail.com>
2025-08-07 01:30:43 -0400

    criado classes de geracao e recuperacao de token e filtros

--- src/main/java/forum/hub/api/controller/AutenticacaoController.java
+++ src/main/java/forum/hub/api/controller/AutenticacaoController.java
@@ -1,6 +1,9 @@
 package forum.hub.api.controller;
 
 import forum.hub.api.domain.usuario.DadosAutenticacao;
+import forum.hub.api.domain.usuario.Usuario;
+import forum.hub.api.infra.security.DadosTokenJWT;
+import forum.hub.api.infra.security.TokenService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -15,11 +18,97 @@ public class AutenticacaoController {
     @Autowired
     private AuthenticationManager manager;
 
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping
     public ResponseEntity efetuarLgin(@RequestBody @Valid DadosAutenticacao dados){
         var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
         var authentication = manager.authenticate(token);
 
-        return ResponseEntity.ok().build();
+        var tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
+
+        return ResponseEntity.ok(new DadosTokenJWT(tokenJWT));
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
